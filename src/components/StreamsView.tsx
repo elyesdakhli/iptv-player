8cd2c2e42f115d3a81b42b1f604d615e6169883d
Elyes <dakhli.elyes@gmail.com>
2025-02-12 13:22:10 +0100

    added film cards.

--- src/components/StreamsView.tsx
+++ src/components/StreamsView.tsx
@@ -1,4 +1,4 @@
-import { Category, Stream} from "../types/Types.ts";
+import {Category, Stream} from "../types/Types.ts";
 import {useContext, useEffect, useState} from "react";
 import {Card, Col, Row} from "react-bootstrap";
 import '../css/streams.css';
@@ -8,6 +8,7 @@ import {LoadingSpinner} from "./common/LoadingSpinner.tsx";
 import {ErrorAlert} from "./common/ErrorAlert.tsx";
 import {SearchBar} from "./common/SearchBar.tsx";
 import {useFetchStreams} from "../hooks/useFetchStreams.ts";
+import fallbackFilmImage from '../assets/film-play-transparant.png';
 
 type StreamsViewProps = {
     category: Category | null;
@@ -47,18 +48,25 @@ function StreamsView ({category, onSelect}: StreamsViewProps) {
             <SearchBar searchFn={handleSearch} searchPlaceHolder="Search channel" />
         </div>
         <Row className="vh-100 g-4 mt-3 vertical-scroll">
-            <StreamItems streams={displayStreams} onSelect={onSelect} />
+            <StreamItems streams={displayStreams} onSelect={onSelect}/>
         </Row>
     </div>
 }
 
 const StreamItems = ({streams, onSelect}: {streams: Stream[] | undefined, onSelect: (stream: Stream) => void}) => {
+    const mode = useContext(ModeContext)
     return (
-        <>
-            {streams?.map((stream, index) => (
-                <StreamCard stream={stream} index={index} key={index} onSelect={onSelect} />
-            ))}
-        </>
+        mode === 'TV' ?
+            <>
+                {streams?.map((stream, index) => (
+                    <TvStreamCard stream={stream} index={index} key={index} onSelect={onSelect} />
+                ))}
+            </> :
+            <>
+                {streams?.map((stream, index) => (
+                    <FilmStreamCard stream={stream}  key={index} onSelect={onSelect} />
+                ))}
+            </>
     );
 }
 
@@ -67,19 +75,40 @@ type StreamCardProps = {
     index: number;
     onSelect: (stream: Stream) => void;
 }
-const StreamCard = ({stream, index, onSelect}: StreamCardProps) => {
+const TvStreamCard = ({stream, index, onSelect}: StreamCardProps) => {
     const [hoveredStreamInd, setHoveredStreamInd] = useState(-1);
 
-    return (<Col xs={12} sm={6} md={4} lg={3}>
-        <Card className={`h-100 ${( hoveredStreamInd == index)? "border-primary shadow-lg" : "border-light"}`}
-              onClick={() => onSelect(stream)}
-              onMouseEnter={() => setHoveredStreamInd(index)}
-              onMouseLeave={() => setHoveredStreamInd(-1)}>
-            <Card.Body>
-                <Card.Body><img src={stream.streamIcon} alt={''} height={25} width={25}/> {stream.name}</Card.Body>
-            </Card.Body>
-        </Card>
-    </Col>)
+    return (
+        <Col xs={12} sm={6} md={4} lg={3}>
+            <Card className={`${( hoveredStreamInd == index)? "border-primary shadow-lg" : "border-light"}`}
+                  onClick={() => onSelect(stream)}
+                  onMouseEnter={() => setHoveredStreamInd(index)}
+                  onMouseLeave={() => setHoveredStreamInd(-1)}>
+                <Card.Body>
+                    <Card.Body><img src={stream.streamIcon} alt={''} height={25} width={25}/> {stream.name}</Card.Body>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
 };
+
+const FilmStreamCard = ({stream, onSelect}: { stream: Stream, onSelect: (stream: Stream) => void }) => {
+    const [error, setError] = useState(false);
+
+    return (
+        <Col xs={12} sm={6} md={4} lg={2}>
+            <Card style={{ cursor: 'pointer' }}
+                  onClick={() => onSelect(stream)}>
+                <Card.Img variant="top"
+                          src={error ? fallbackFilmImage : stream.streamIcon}
+                          onError={() => setError(true)}
+                    />
+                <Card.Body>
+                    <Card.Title>{stream.name}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
 
 export default StreamsView;
