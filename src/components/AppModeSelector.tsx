02588f8588e42f464f115ac4e935673dea7406f8
Elyes <dakhli.elyes@gmail.com>
2025-02-13 20:07:24 +0100

    added gh pages and small fixes

--- src/components/AppModeSelector.tsx
+++ src/components/AppModeSelector.tsx
@@ -1,19 +1,23 @@
 import {AppMode} from "../types/Types.ts";
 import {Dropdown, DropdownButton} from "react-bootstrap";
-import {useState} from "react";
+import {useContext, useState} from "react";
+import {SourceContext} from "../context/SourceContext.ts";
 
 export const AppModeSelector = ({onSelect}: {onSelect: (mode: AppMode) => void}) => {
     const [mode, setMode] = useState('TV');
+    const source = useContext(SourceContext);
 
     return (
-        <DropdownButton title={mode ||"Mode" } onSelect={(event) => {
-                setMode(event as AppMode);
-                onSelect(event as AppMode)
-              }
-            } variant="success">
-            <Dropdown.Item eventKey="TV">TV</Dropdown.Item>
-            <Dropdown.Item eventKey="FILMS">Films</Dropdown.Item>
-            <Dropdown.Item eventKey="SERIES">Series</Dropdown.Item>
-        </DropdownButton>
+        source && (
+            <DropdownButton title={mode ||"Mode" } onSelect={(event) => {
+                    setMode(event as AppMode);
+                    onSelect(event as AppMode)
+                  }
+                } variant="success">
+                <Dropdown.Item eventKey="TV">TV</Dropdown.Item>
+                <Dropdown.Item eventKey="FILMS">Films</Dropdown.Item>
+                <Dropdown.Item eventKey="SERIES">Series</Dropdown.Item>
+            </DropdownButton>
+        )
     )
 }
