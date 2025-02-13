3414927b971cd039ed1095b5c68b1db8a17b6514
Elyes <dakhli.elyes@gmail.com>
2025-02-13 20:43:36 +0100

    revert appModeSelector conditional display

--- src/components/AppModeSelector.tsx
+++ src/components/AppModeSelector.tsx
@@ -1,23 +1,19 @@
 import {AppMode} from "../types/Types.ts";
 import {Dropdown, DropdownButton} from "react-bootstrap";
-import {useContext, useState} from "react";
-import {SourceContext} from "../context/SourceContext.ts";
+import {useState} from "react";
 
 export const AppModeSelector = ({onSelect}: {onSelect: (mode: AppMode) => void}) => {
     const [mode, setMode] = useState('TV');
-    const source = useContext(SourceContext);
 
     return (
-        source && (
-            <DropdownButton title={mode ||"Mode" } onSelect={(event) => {
-                    setMode(event as AppMode);
-                    onSelect(event as AppMode)
-                  }
-                } variant="success">
-                <Dropdown.Item eventKey="TV">TV</Dropdown.Item>
-                <Dropdown.Item eventKey="FILMS">Films</Dropdown.Item>
-                <Dropdown.Item eventKey="SERIES">Series</Dropdown.Item>
-            </DropdownButton>
-        )
+        <DropdownButton title={mode ||"Mode" } onSelect={(event) => {
+                setMode(event as AppMode);
+                onSelect(event as AppMode)
+              }
+            } variant="success">
+            <Dropdown.Item eventKey="TV">TV</Dropdown.Item>
+            <Dropdown.Item eventKey="FILMS">Films</Dropdown.Item>
+            <Dropdown.Item eventKey="SERIES">Series</Dropdown.Item>
+        </DropdownButton>
     )
 }
